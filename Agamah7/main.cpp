8398a406dfdcba3145bba80bf97549b8f53d6edb
mickeyN007 <michaeloduah@Michaels-MacBook-Pro.local>
2016-04-08 22:16:51 -0500

    General Debugging - 1

--- Agamah7/main.cpp
+++ Agamah7/main.cpp
@@ -5,20 +5,20 @@
 
 using namespace std;
 
-double getAverage(const personType human[], int countVal)
-void read(summaryType& testsummary, personType human[], int& count)
-float getMedian(const personType human[], int countVal)
-void sort(personType human[], int n)
+double getAverage(const personType human[], int countVal);
+void read(summaryType& testsummary, personType human[], int& count);
+float getMedian(const personType human[], int countVal);
+void sort(personType human[], int n);
 
 
 int main()
 {
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
 }
--- Agamah7/read.cpp
+++ Agamah7/read.cpp
@@ -1,13 +1,18 @@
+#include "decl.h"
+#include <fstream>
+#include <iostream>
+using namespace std;
 void read(summaryType& testsummary, personType human[], int& count)
 {
-   int num;
+  string name;
+   int age;
    count=0;
    ifstream infile;
    infile.open("data");
    if (infile.is_open())
    {
-      infile >> human[count].age;
-       while (!infile.eof() && count<arraysize)  // same as while (!infile.eof()
+      infile >> name;//human[count].age;
+       while (!infile.eof() )//&& count<arraysize)  // same as while (!infile.eof()
 //      while (infile && count<arraysize)  // works for '\n' after last line or not
 //      while (!infile.eof() && count<arraysize)   // same as while (infile.good()
       {
@@ -15,7 +20,8 @@ void read(summaryType& testsummary, personType human[], int& count)
 	 human[count].name = name;  // best to never read directly into an array
 	 infile.ignore(80, '\n');
 	 count++;
-	 infile >> human[count].age;
+	 infile >> age;
+   human[count].age = age;
       }
       infile.close();
       //cout << "count = " << count << endl;
@@ -23,4 +29,4 @@ void read(summaryType& testsummary, personType human[], int& count)
    else
       cout << "error opening 'data'\n";
 }
-   
+
